01fc71d48ba230ce82224d141cfe1ef6b43463df
Tiago Gomes <tidgomes@gmail.com>
2023-03-22 07:32:13 -0300

    refactor the state management to a specific and centralized ContextProvider

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const AuthContext = React.createContext({
-  isLoggedIn: false
+  isLoggedIn: false,
+  onLogin: () => {},
+  onLogout: () => {}
 })
+
+export const AuthContextProvider = props => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn')
+
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true)
+    }
+  }, [])
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1')
+    setIsLoggedIn(true)
+  }
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
+    setIsLoggedIn(false)
+  }
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn, onLogin: loginHandler, onLogout: logoutHandler }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
 
 export default AuthContext
